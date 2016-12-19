2b3fd4eeb784ebfdc69d96be06a9297d57e0192b
赵晓东 <zhaoxiaodong@innobuddy.com>
2016-12-19 19:05:29 +0800

    format

--- leetcode/283-move-zeros.js
+++ leetcode/283-move-zeros.js
@@ -8,28 +8,20 @@
 
 let moveZeros = function(nums){
   let i = 0,j = 1, len = nums.length;
-  
   if(nums.length < 1){
     return nums[0] === 0 ? [nums[1],0] : nums;
   }
-  
   for(i,j; j < len; ){
     if(nums[i] === 0 && nums[j] !== 0){
       let tmp = nums[j];
       nums[j] = nums[i];
       nums[i] = tmp;
-      i++;
-      j++;
+      i++; j++;
       continue;
     }
-    
-    while(nums[i] !== 0 && i < len){
-      i++ ;
-    }
+    while(nums[i] !== 0 && i < len){ i++ ; }
     j = i + 1;
-    while (nums[j] === 0 && j < len ) {
-      j++ ;
-    }
+    while (nums[j] === 0 && j < len ) { j++ ; }
   }
   console.log(nums);
 }
