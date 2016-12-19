c491a8e38c51259d1c3d2471adb514afde9de1c2
ZhaoXiaodong <you_shoubian@163.com>
2016-12-19 18:43:53 +0800

    Create move-zeros.js
    leetcode 283

---
+++ leetcode/move-zeros.js
@@ -0,0 +1,37 @@
+/*
+* leetcode
+* 283. Move Zeroes  AC
+* zhao xiaodong 2016-12-19
+*/
+
+'use strict';
+
+let moveZeros = function(nums){
+  let i = 0,j = 1, len = nums.length;
+  
+  if(nums.length < 1){
+    return nums[0] === 0 ? [nums[1],0] : nums;
+  }
+  
+  for(i,j; j < len; ){
+    if(nums[i] === 0 && nums[j] !== 0){
+      let tmp = nums[j];
+      nums[j] = nums[i];
+      nums[i] = tmp;
+      i++;
+      j++;
+      continue;
+    }
+    
+    while(nums[i] !== 0 && i < len){
+      i++ ;
+    }
+    j = i + 1;
+    while (nums[j] === 0 && j < len ) {
+      j++ ;
+    }
+  }
+  console.log(nums);
+}
+
+moveZeros(process.argv.slice(2).map(item => {return Number(item)}));
